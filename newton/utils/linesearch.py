36da736256ef5d7e2c57c3aea54f80d5b4fecb85
GiacomoCorradini <corra981@gmail.com>
2026-05-06 15:45:24 +0200

    feat: update linesearch with merit-l1

--- newton/utils/linesearch.py
+++ newton/utils/linesearch.py
@@ -1,26 +1,32 @@
+import numpy as np
+
+
 def warning_alpha_min(alpha_min: float, method_name: str) -> None:
     print(
         f"Warning: {method_name} linesearch failed to find a suitable step size. Minimum alpha reached: {alpha_min}"
     )
 
 
-def backtracking_linesearch(
-    f, x, Dx, alpha: float, beta: float, alpha_min: float = 1e-8
-) -> float:
-    while f(x + alpha * Dx) >= f(x):
-        alpha = beta * alpha
-
-        if alpha < alpha_min:
-            warning_alpha_min(alpha, "Backtracking")
-            break
-
-    return alpha
-
-
 def armijo_linesearch(
-    f, x, Dx, dfx, alpha: float, beta: float, gamma: float, alpha_min: float = 1e-8
+    f,
+    g,
+    x,
+    Dx,
+    dfx,
+    beta: float,
+    gamma: float,
+    sigma: float,
+    alpha: float = 1.0,
+    alpha_min: float = 1e-8,
 ) -> float:
-    while f(x + alpha * Dx) >= f(x) + gamma * alpha * dfx.T @ Dx:
+    f_x = f(x)
+    g_x = sigma * np.linalg.norm(g(x), ord=1)
+    Ddx = dfx.T @ Dx - g_x
+
+    while (
+        f(x + alpha * Dx) + sigma * np.linalg.norm(g(x + alpha * Dx), ord=1)
+        >= f_x + g_x + gamma * alpha * Ddx
+    ):
         alpha = beta * alpha
 
         if alpha < alpha_min:
